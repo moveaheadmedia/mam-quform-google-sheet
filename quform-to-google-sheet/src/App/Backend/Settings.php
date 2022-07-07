6ba1b5b5a2aea2604b274e73d4fe1d2df84a7f86
Ali Sal <ali@moveaheadmedia.co.uk>
2022-07-07 15:37:46 +0700

    Adding Options Page

--- quform-to-google-sheet/src/App/Backend/Settings.php
+++ quform-to-google-sheet/src/App/Backend/Settings.php
@@ -9,7 +9,7 @@
  * @link      https://salti-media.com
  */
 
-declare( strict_types = 1 );
+declare(strict_types=1);
 
 namespace QuformToGoogleSheet\App\Backend;
 
@@ -21,21 +21,46 @@ use QuformToGoogleSheet\Common\Abstracts\Base;
  * @package QuformToGoogleSheet\App\Backend
  * @since 1.0.0
  */
-class Settings extends Base {
+class Settings extends Base
+{
 
-	/**
-	 * Initialize the class.
-	 *
-	 * @since 1.0.0
-	 */
-	public function init() {
-		/**
-		 * This backend class is only being instantiated in the backend as requested in the Bootstrap class
-		 *
-		 * @see Requester::isAdminBackend()
-		 * @see Bootstrap::__construct
-		 *
-		 * Add plugin code here for admin settings specific functions
-		 */
-	}
+    /**
+     * Initialize the class.
+     *
+     * @since 1.0.0
+     */
+    public function init()
+    {
+        /**
+         * This backend class is only being instantiated in the backend as requested in the Bootstrap class
+         *
+         * @see Requester::isAdminBackend()
+         * @see Bootstrap::__construct
+         *
+         */
+        add_action('init', [$this, 'add_option_page']);
+        add_action('init', [$this, 'add_option_page_acf']);
+    }
+
+    public function add_option_page()
+    {
+        if (function_exists('acf_add_options_page')) {
+            acf_add_options_page([
+                'page_title' => 'Quform To Google Sheet',
+                'menu_title' => 'Quform To Google Sheet',
+                'menu_slug' => 'quform-to-google-sheet',
+                'capability' => 'edit_posts',
+                'icon_url' => 'dashicons-editor-table',
+                'redirect' => false,
+            ]);
+        }
+    }
+
+    public function add_option_page_acf()
+    {
+        if (function_exists('acf_add_local_field_group')) {
+            $acf = json_decode(file_get_contents(plugin_dir_path(QUFORM_TO_GOOGLE_SHEET_PLUGIN_FILE) . '/assets/admin/acf.json'), true);
+            acf_add_local_field_group($acf[0]);
+        }
+    }
 }
