96b11c65d4129a14495e4bd307ae38dbe936714f
Ali Sal <ali@moveaheadmedia.co.uk>
2022-07-07 15:32:05 +0700

    Adding Requirements

--- quform-to-google-sheet/src/Config/Requirements.php
+++ quform-to-google-sheet/src/Config/Requirements.php
@@ -9,7 +9,7 @@
  * @link      https://salti-media.com
  */
 
-declare( strict_types = 1 );
+declare(strict_types=1);
 
 namespace QuformToGoogleSheet\Config;
 
@@ -25,101 +25,108 @@ use QuformToGoogleSheet\Common\Utils\Errors;
  * @package QuformToGoogleSheet\Config
  * @since 1.0.0
  */
-final class Requirements extends Base {
-	/**
-	 * Specifications for the requirements
-	 *
-	 * @return array : used to specify the requirements
-	 * @since 1.0.0
-	 */
-	public function specifications(): array {
-		return apply_filters( 'quform_to_google_sheet_plugin_requirements', [
-			'php'            => $this->plugin->requiredPhp(),
-			'php_extensions' => [
-				/**
-				 * 'mbstring'
-				 */
-			],
-			'wp'             => $this->plugin->requiredWp(),
-			'plugins'        => [
-				/**
-				 * [
-				 *  'file'    => 'hello-dolly/hello.php',
-				 *  'name'    => 'Hello Dolly',
-				 *  'version' => '1.5'
-				 * ],
-				 */
-			],
-		] );
-	}
+final class Requirements extends Base
+{
+    /**
+     * Specifications for the requirements
+     *
+     * @return array : used to specify the requirements
+     * @since 1.0.0
+     */
+    public function specifications(): array
+    {
+        return apply_filters('quform_to_google_sheet_plugin_requirements', [
+            'php' => $this->plugin->requiredPhp(),
+            'php_extensions' => [
+                /**
+                 * 'mbstring'
+                 */
+            ],
+            'wp' => $this->plugin->requiredWp(),
+            'plugins' => [
+                [
+                    'file' => 'advanced-custom-fields-pro/acf.php',
+                    'name' => 'Advanced Custom Fields PRO',
+                    'version' => '5'
+                ],
+                [
+                    'file' => 'quform/quform.php',
+                    'name' => 'Quform',
+                    'version' => '2'
+                ],
+            ],
+        ]);
+    }
 
-	/**
-	 * Plugin requirements checker
-	 *
-	 * @since 1.0.0
-	 */
-	public function check() {
-		// We use "Requirements" if the package is required and installed by composer.json
-		if ( class_exists( '\Micropackage\Requirements\Requirements' ) ) {
-			$this->requirements = new \Micropackage\Requirements\Requirements(
-				$this->plugin->name(),
-				$this->specifications()
-			);
-			if ( ! $this->requirements->satisfied() ) {
-				// Print notice
-				$this->requirements->print_notice();
-				// Kill plugin
-				Errors::pluginDie();
-			}
-		} else {
-			// Else we do a version check based on version_compare
-			$this->versionCompare();
-		}
-	}
+    /**
+     * Plugin requirements checker
+     *
+     * @since 1.0.0
+     */
+    public function check()
+    {
+        // We use "Requirements" if the package is required and installed by composer.json
+        if (class_exists('\Micropackage\Requirements\Requirements')) {
+            $this->requirements = new \Micropackage\Requirements\Requirements(
+                $this->plugin->name(),
+                $this->specifications()
+            );
+            if (!$this->requirements->satisfied()) {
+                // Print notice
+                $this->requirements->print_notice();
+                // Kill plugin
+                Errors::pluginDie();
+            }
+        } else {
+            // Else we do a version check based on version_compare
+            $this->versionCompare();
+        }
+    }
 
-	/**
-	 * Compares PHP & WP versions and kills plugin if it's not compatible
-	 *
-	 * @since 1.0.0
-	 */
-	public function versionCompare() {
-		foreach (
-			[
-				// PHP version check
-				[
-					'current' => phpversion(),
-					'compare' => $this->plugin->requiredPhp(),
-					'title'   => __( 'Invalid PHP version', 'quform-to-google-sheet' ),
-					'message' => sprintf( /* translators: %1$1s: required php version, %2$2s: current php version */
-						__( 'You must be using PHP %1$1s or greater. You are currently using PHP %2$2s.', 'quform-to-google-sheet' ),
-						$this->plugin->requiredPhp(),
-						phpversion()
-					),
-				],
-				// WP version check
-				[
-					'current' => get_bloginfo( 'version' ),
-					'compare' => $this->plugin->requiredWp(),
-					'title'   => __( 'Invalid WordPress version', 'quform-to-google-sheet' ),
-					'message' => sprintf( /* translators: %1$1s: required wordpress version, %2$2s: current wordpress version */
-						__( 'You must be using WordPress %1$1s or greater. You are currently using WordPress %2$2s.', 'quform-to-google-sheet' ),
-						$this->plugin->requiredWp(),
-						get_bloginfo( 'version' )
-					),
-				],
-			] as $compat_check ) {
-			if ( version_compare(
-				$compat_check['compare'],
-				$compat_check['current'],
-				'>='
-			) ) {
-				// Kill plugin
-				Errors::pluginDie(
-					$compat_check['message'],
-					$compat_check['title'],
-					plugin_basename( __FILE__ )
-				);
-			}
-		}
-	}
+    /**
+     * Compares PHP & WP versions and kills plugin if it's not compatible
+     *
+     * @since 1.0.0
+     */
+    public function versionCompare()
+    {
+        foreach (
+            [
+                // PHP version check
+                [
+                    'current' => phpversion(),
+                    'compare' => $this->plugin->requiredPhp(),
+                    'title' => __('Invalid PHP version', 'quform-to-google-sheet'),
+                    'message' => sprintf( /* translators: %1$1s: required php version, %2$2s: current php version */
+                        __('You must be using PHP %1$1s or greater. You are currently using PHP %2$2s.', 'quform-to-google-sheet'),
+                        $this->plugin->requiredPhp(),
+                        phpversion()
+                    ),
+                ],
+                // WP version check
+                [
+                    'current' => get_bloginfo('version'),
+                    'compare' => $this->plugin->requiredWp(),
+                    'title' => __('Invalid WordPress version', 'quform-to-google-sheet'),
+                    'message' => sprintf( /* translators: %1$1s: required wordpress version, %2$2s: current wordpress version */
+                        __('You must be using WordPress %1$1s or greater. You are currently using WordPress %2$2s.', 'quform-to-google-sheet'),
+                        $this->plugin->requiredWp(),
+                        get_bloginfo('version')
+                    ),
+                ],
+            ] as $compat_check) {
+            if (version_compare(
+                $compat_check['compare'],
+                $compat_check['current'],
+                '>='
+            )) {
+                // Kill plugin
+                Errors::pluginDie(
+                    $compat_check['message'],
+                    $compat_check['title'],
+                    plugin_basename(__FILE__)
+                );
+            }
+        }
+    }
 }
